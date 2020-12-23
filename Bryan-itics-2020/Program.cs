f0c242a06ed63966a142ef8934555ee9806eafee
Bryan-BV <bryanbrr96@gmail.com>
2020-12-22 20:18:48 -0600

    Se subieron las lineas de salida que no estan en el commit anterior

--- Bryan-itics-2020/Program.cs
+++ Bryan-itics-2020/Program.cs
@@ -7,6 +7,8 @@ namespace Bryan_itics_2020
         static void Main(string[] args)
         {
             Console.WriteLine("Inicio del Proyecto de Git y GitHub");
+            Console.WriteLine("Se creo la rama: Otra-rama");
+            Console.WriteLine("solo para verificacion de subir una rama del ropositorio local hacia el remoto");
         }
     }
 }
