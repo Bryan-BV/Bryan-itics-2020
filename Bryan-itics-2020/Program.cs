f1ae4360d0b871cf0533949d616e4ada215371c3
Bryan-BV <bryanbrr96@gmail.com>
2020-12-22 21:10:51 -0600

    Se creo un objeto de tipo Director

--- Bryan-itics-2020/Program.cs
+++ Bryan-itics-2020/Program.cs
@@ -7,6 +7,10 @@ namespace Bryan_itics_2020
         static void Main(string[] args)
         {
             Console.WriteLine("Inicio del Proyecto de Git y GitHub");
+            Director director = new Director("Liceo Guatemala", 6000, "Jorge Mendez", 40, 'M');
+            director.DatosPersona();
+            director.Tareas();
+            Console.WriteLine("==================================================================");
         }
     }
 }
