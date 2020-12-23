164a99b712374ae7cb7b13ccfa1621f1c3990218
Bryan-BV <bryanbrr96@gmail.com>
2020-12-22 20:51:09 -0600

    Se completo la clase Director, con variables, propiedades y metodos

--- Bryan-itics-2020/Director.cs
+++ Bryan-itics-2020/Director.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace Bryan_itics_2020
 {
-    class Director
+    class Director : PersonaAcademica
     {
+        string escuela;
+        double sueldo;
+
+        public Director()
+        {
+
+        }
+
+        public Director(string escuela, double sueldo, string nombre, int edad, char genero) : base(nombre, edad, genero)
+        {
+            this.escuela = escuela;
+            this.sueldo = sueldo;
+        }
+
+        public string Escuela { get => escuela; set => escuela = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+
+        public override void DatosPersona()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}, Genero: {Genero}, Salario: {Sueldo}");
+        }
+
+        public override void Tareas()
+        {
+            Console.WriteLine($"Yo {Nombre}, soy el Dirctor de la escula: {Escuela}");
+        }
     }
 }
