dc31bc7344028f0e49521e083d77bdd7b1bf161a
Bryan-BV <bryanbrr96@gmail.com>
2020-12-22 18:53:07 -0600

    Se agrego la clase Profesor y se corrigieron algunos errores

---
+++ Bryan-itics-2020/Profesor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bryan_itics_2020
+{
+    class Profesor : PersonaAcademica
+    {
+        string curosoimparte;
+        double sueldo;
+
+        public Profesor()
+        {
+
+        }
+
+        public Profesor(string curosoimparte, double sueldo, string nombre, int edad, char genero) : base(nombre, edad, genero)
+        {
+            this.curosoimparte = curosoimparte;
+            this.sueldo = sueldo;
+        }
+
+        public string Curosoimparte { get => curosoimparte; set => curosoimparte = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+
+        public override void DatosPersona()
+        {
+            Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}, Genero: {Genero}, Curso que imparte: {Curosoimparte}, Salario: {Sueldo}");
+        }
+
+        public override void Tareas()
+        {
+            Console.WriteLine($"Yo {Nombre}, soy un(a) profesor(a) y imparto el curso de: {Curosoimparte}");
+        }
+    }
+
+}
